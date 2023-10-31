5fce105b1b059a370c134f72d7f231c8c5afb628
André Miragaia <andre.miragaia@ua.pt>
2023-10-31 15:29:49 +0000

    up

--- MPEI/Guiao4/EX5.m
+++ MPEI/Guiao4/EX5.m
@@ -4,7 +4,7 @@ clc;
 %% a)
 T = [0.7 0.2 0.3;
      0.2 0.3 0.3;
-     0.1 0.5 0.4];
+     0.1 0.5 0.4]';
 
 %% b)
 % 1 no sol e 0 nos restantes
@@ -13,3 +13,7 @@ x1 = T^2*x0;
 fprintf("A probabilidade de estar sol no segundo e no terceiro dia, ");
 
 %% c)
+sol = 1;
+nuvens = 2;
+chuva = 3;
+p_c = T(sol,sol)*T(sol,sol)+T(nuvens,sol)*T(sol,nuvens)+T(nuvens,sol)*T(nuvens,nuvens)+T(sol,sol)*T(nuvens,sol)
